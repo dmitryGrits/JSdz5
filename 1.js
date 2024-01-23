0f033d685cfa0060bbda8778d1160ff53d0eba88
DGRITS <grizay@icloud.com>
2024-01-23 21:25:46 +0300

    2 task

--- 1.js
+++ 1.js
@@ -5,15 +5,15 @@
 Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
 */
 
-const numbers = {
-  key1: 12,
-  key2: 2,
-  key3: 4,
-  key4: 1,
-  key5: -244,
-  key6: 0,
-  key7: 7,
-};
+// const numbers = {
+//   key1: 12,
+//   key2: 2,
+//   key3: 4,
+//   key4: 1,
+//   key5: -244,
+//   key6: 0,
+//   key7: 7,
+// };
 
-const arrNum = Object.values(numbers);
-console.log(arrNum.filter((num) => num >= 3));
+// const arrNum = Object.values(numbers);
+// console.log(arrNum.filter((num) => num >= 3));
